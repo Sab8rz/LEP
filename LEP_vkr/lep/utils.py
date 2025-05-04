dd8274ce15e9d88fe4e6cef1cd5ad4374b6993bc
Sab8rz <qwartopl@mail.ru>
2025-05-04 15:44:14 +0300

    ФИКС

--- LEP_vkr/lep/utils.py
+++ LEP_vkr/lep/utils.py
@@ -14,7 +14,7 @@ class LepCalculator:
         self.F0 = self.wire.gen_cross_sec  # общее сечение
         self.d = self.wire.diametr  # диаметр
         self.p = self.wire.weight * 9.80665 / 1000  # вес
-        self.a0 = self.wire.coef_lin_exp * 10**-6  # коэффициент линейного расширения
+        self.a0 = self.wire.coef_lin_exp * 1e-6  # коэффициент линейного расширения
         self.E0 = self.wire.mod_elast_mat  # модуль упругости материала
         self.o_r = self.wire.max_vol  # допустимое напряжение при наибольших нагрузках
         self.o_h = self.o_r  # допустимое напряжение при низших температурах = наибольших нагрузках
@@ -146,7 +146,7 @@ class LepCalculatorManual(LepCalculator):
         self.F0 = F0
         self.d = diameter
         self.p = weight * 9.80665 / 1000
-        self.a0 = a0 * 10**-6
+        self.a0 = a0 * 1e-6
         self.E0 = E0
         self.o_r = o_r
         self.o_h = self.o_r
